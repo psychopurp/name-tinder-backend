bf9228cca43b69b5c019699b5e33fc7e7eb50e6a
smackgg <smackgg@gmail.com>
2019-05-22 15:00:11 +0800

    add api

--- routes/other.js
+++ routes/other.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
 const {
   getNames,
+  likeName,
 } = require('../controller/other/name')
 
 const {
@@ -8,15 +9,19 @@ const {
   exitGroup,
   joinGroup,
   getGroupDetail,
+  getGroups,
 } = require('../controller/other/groups')
 
 router.prefix('/api')
 
 router.get('/names', ctx => getNames(ctx))
+router.put('/name/like', ctx => likeName(ctx))
+
 
 router.get('/group/create', ctx => createGroups(ctx))
 router.get('/group/exit', ctx => exitGroup(ctx))
 router.get('/group/join', ctx => joinGroup(ctx))
 router.get('/group', ctx => getGroupDetail(ctx))
+router.get('/groups', ctx => getGroups(ctx))
 
 module.exports = router
