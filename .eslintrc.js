982f598979c681a3910ac2b5f698fffdf387d11e
Elyar <1595685558@qq.com>
2019-11-24 17:58:50 +0800

    .

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,42 @@
-module.exports = {
-  extends: "airbnb-base",
-  rules: {
-    camelcase: 0,
-    "new-cap": ["error", { capIsNew: false }],
-    "no-console": 0,
-    radix: ["error", "as-needed"],
-    "linebreak-style": 0,
-    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
-    "arrow-parens": 0,
-    semi: [0, "never"],
-    "comma-dangle": [0, "always-multiline"],
-    "max-len": 0,
-    "space-before-function-paren": [0, "always"],
-    "no-unused-expressions": [
-      0,
-      {
-        allowShortCircuit: true,
-        allowTernary: true
-      }
-    ],
-    "arrow-body-style": [0, "never"],
-    "func-names": 0,
-    "prefer-const": 0,
-    "no-extend-native": 0,
-    "no-param-reassign": 0,
-    "no-restricted-syntax": 0,
-    "no-await-in-loop": 0,
-    "no-useless-escape": 0,
-    "no-eval": 0,
-    "no-continue": 0,
-    "no-unused-vars": [0, { ignoreRestSiblings: true }],
-    "no-underscore-dangle": 0,
-    "global-require": 0,
-    quotes: 0,
-    "import/no-unresolved": 0,
-    "import/extensions": 0,
-    "import/no-extraneous-dependencies": 0,
-    "import/prefer-default-export": 0,
-    "prefer-destructuring": 0
-  }
-};
+// module.exports = {
+//   extends: "airbnb-base",
+//   rules: {
+//     //   camelcase: 0,
+//     //   "new-cap": ["error", { capIsNew: false }],
+//     //   "no-console": 0,
+//     //   radix: ["error", "as-needed"],
+//     //   "linebreak-style": 0,
+//     //   "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+//     //   "arrow-parens": 0,
+//     //   semi: [0, "never"],
+//     //   "comma-dangle": [0, "always-multiline"],
+//     //   "max-len": 0,
+//     //   "space-before-function-paren": [0, "always"],
+//     //   "no-unused-expressions": [
+//     //     0,
+//     //     {
+//     //       allowShortCircuit: true,
+//     //       allowTernary: true
+//     //     }
+//     //   ],
+//     //   "arrow-body-style": [0, "never"],
+//     //   "func-names": 0,
+//     //   "prefer-const": 0,
+//     //   "no-extend-native": 0,
+//     //   "no-param-reassign": 0,
+//     //   "no-restricted-syntax": 0,
+//     //   "no-await-in-loop": 0,
+//     //   "no-useless-escape": 0,
+//     //   "no-eval": 0,
+//     //   "no-continue": 0,
+//     //   "no-unused-vars": [0, { ignoreRestSiblings: true }],
+//     //   "no-underscore-dangle": 0,
+//     //   "global-require": 0,
+//     //   quotes: 0,
+//     //   "import/no-unresolved": 0,
+//     //   "import/extensions": 0,
+//     //   "import/no-extraneous-dependencies": 0,
+//     //   "import/prefer-default-export": 0,
+//     //   "prefer-destructuring": 0
+//   }
+// };
